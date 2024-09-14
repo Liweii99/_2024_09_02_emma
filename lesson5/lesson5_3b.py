95c24fcca08c046c6c24085c271154cf9be40a43
liweii99 <liweii9988@gmail.com>
2024-09-14 15:12:22 +0800

    liweii99

--- lesson5/lesson5_3b.py
+++ lesson5/lesson5_3b.py
@@ -1,4 +1,4 @@
-def input_data():
+def input_data()->tuple[int,int]:
     while True:
         try:    
             cm = int(input("請輸入身高(公分):"))
@@ -26,27 +26,34 @@ def input_data():
             continue
     return cm,kg
 
+def get_status(bmi:float)->str:
+    if BMI >=35:
+        return "重度肥胖：BMI≧35"
+    elif BMI >=30:
+        return "中度肥胖：30≦BMI"
+    elif BMI >=27:
+        return "輕度肥胖：27≦BMI"
+    elif BMI >=24:
+        return "過重"
+    elif BMI >=18.5:
+        return "正常範圍"
+    else:
+        return "體重過輕"
+
+def calculate_bmi(kg:int, cm:int) -> float:
+    cm=(cm/100)*(cm/100)
+    BMI=kg/cm
+    return BMI
+
 while True:
     kg=0  #清除變數
     cm=0  #清除變數
     cm,kg = input_data() #呼叫function
 
     print(f'身高={cm},體重={kg}')
-    cm=(cm/100)*(cm/100)
-    BMI=kg/cm
+    BMI = calculate_bmi(cm=cm,kg=kg) #引數名稱的呼叫,可以不依照順序
     print(f'BMI={BMI}')
-    if BMI >=35:
-        print("重度肥胖：BMI≧35")
-    elif BMI >=30:
-        print("中度肥胖：30≦BMI")
-    elif BMI >=27:
-        print("輕度肥胖：27≦BMI")
-    elif BMI >=24:
-        print("過重")
-    elif BMI >=18.5:
-        print("正常範圍")
-    else:
-        print("體重過輕")
+    print(get_status(BMI))
     
     play_again = input("還要繼續嗎?(y,n)")
     if play_again == "n":
--- lesson5/widget/__init__.py
+++ lesson5/widget/__init__.py
@@ -1,4 +1,4 @@
-def input_data():
+def input_data()->tuple[int,int]:
     while True:
         try:    
             cm = int(input("請輸入身高(公分):"))
@@ -26,29 +26,21 @@ def input_data():
             continue
     return cm,kg
 
-while True:
-    kg=0  #清除變數
-    cm=0  #清除變數
-    cm,kg = input_data() #呼叫function
+def get_status(bmi:float)->str:
+    if bmi>=35:
+        return "重度肥胖：BMI≧35"
+    elif bmi >=30:
+        return "中度肥胖：30≦BMI"
+    elif bmi >=27:
+        return "輕度肥胖：27≦BMI"
+    elif bmi >=24:
+        return "過重"
+    elif bmi >=18.5:
+        return "正常範圍"
+    else:
+        return "體重過輕"
 
-    print(f'身高={cm},體重={kg}')
+def calculate_bmi(kg:int, cm:int) -> float:
     cm=(cm/100)*(cm/100)
     BMI=kg/cm
-    print(f'BMI={BMI}')
-    if BMI >=35:
-        print("重度肥胖：BMI≧35")
-    elif BMI >=30:
-        print("中度肥胖：30≦BMI")
-    elif BMI >=27:
-        print("輕度肥胖：27≦BMI")
-    elif BMI >=24:
-        print("過重")
-    elif BMI >=18.5:
-        print("正常範圍")
-    else:
-        print("體重過輕")
-    
-    play_again = input("還要繼續嗎?(y,n)")
-    if play_again == "n":
-        break
-print('程式結束')
+    return BMI
