54a4b9a609f5ea17da8aa1f95e10cbafe05579c3
黃力威 <liweii9988@gmail.com>
2024-09-18 03:20:14 +0000

    liweii99

--- lesson5/lesson5_3bb.py
+++ lesson5/lesson5_3bb.py
@@ -48,12 +48,12 @@ def calculate_bmi(kg:int, cm:int) -> float:
 while True:
     kg=0  #清除變數
     cm=0  #清除變數
-    cm,kg = input_data() #呼叫function
+    cm,kg = input_data() #呼叫function,輸入資料用
 
     print(f'身高={cm},體重={kg}')
     BMI = calculate_bmi(cm=cm,kg=kg) #引數名稱的呼叫,可以不依照順序
     print(f'BMI={BMI}')
-    print(get_status(BMI)) #918_2
+    print(get_status(BMI)) #918_2傳出狀態
     
     play_again = input("還要繼續嗎?(y,n)")
     if play_again == "n":
