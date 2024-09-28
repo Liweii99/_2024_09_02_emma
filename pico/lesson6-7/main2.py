5c6c4a2eb4e43b4ae3e343763e396c0aa8b50a8d
liweii99 <liweii9988@gmail.com>
2024-09-28 15:15:47 +0800

    liweii99

--- pico/lesson6-7/main2.py
+++ pico/lesson6-7/main2.py
@@ -1,14 +1,16 @@
 from machine import Timer,ADC,Pin,PWM,RTC
 
-adc = machine.ADC(4)
-PWM = PWM(Pin(15),freq=50)
+
+
+adc = ADC(4)
+pwm = PWM(Pin(15),freq=50)
 conversion_factor = 3.3 / (65535)
 rtc = RTC()
 
 def do_thing(t):
     reading = adc.read_u16() * conversion_factor
-    temperature = 27 - (reading-0.706) / 0.001721
-    year,month,day,weekly,hours,minuse,seconds,info =rtc.datetime()
+    temperature = 27 - (reading - 0.706)/0.001721
+    year,month,day,weekly,hours,minuse,seconds,info = rtc.datetime()
     datetime_str = f"{year}-{month}-{day} {hours}:{minuse}:{seconds}"
     print(datetime_str)
     print(temperature)
@@ -16,9 +18,11 @@ def do_thing(t):
 def do_thing1(t):
     adc1 = ADC(Pin(26))
     duty = adc1.read_u16()
-    PWM.duty_u16(duty)
+    pwm.duty_u16(duty)
     
     print(f'可變電阻:{round(duty/65535*10)}')
-    
+
+
+
 t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
 t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1)
