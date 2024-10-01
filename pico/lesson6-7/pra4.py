0f1c21bfe340becef91865c8f680ae7fa7dab4fa
黃力威 <liweii9988@gmail.com>
2024-10-02 07:53:07 +0800

    emma

---
+++ pico/lesson6-7/pra4.py
@@ -0,0 +1,23 @@
+from machine import Timer,ADC,Pin,PWM
+
+adc = ADC(4)
+pwm = PWM(Pin(15),freq=50)
+conversion_factor = 3.3/(65535)
+
+def do_thing(t):
+    reading = adc.read_u16() * conversion_factor
+    temperature = 27 - (reading - 0.706)/0.001721
+    print(temperature)
+    
+def do_thing1(t):
+    adc1 = ADC(Pin(26))
+    duty = adc1.read_u16()
+    pwm.duty_u16(duty) #燈可調整亮度
+    
+    print(f'可變電阻:{round(duty/65535*10)}')#round取整數
+    
+t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
+t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1)
+
+
+
