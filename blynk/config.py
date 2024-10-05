11eac9941479b075bcd01f9bd92d7aec7954dc32
黃力威 <liweii9988@gmail.com>
2024-10-05 18:52:03 +0800

    emma

--- blynk/config.py
+++ blynk/config.py
@@ -1,7 +1,7 @@
 # 在此填入 Blynk Device Info 的資訊
-BLYNK_TEMPLATE_ID   = "TMPL6cI9rdXii"
+BLYNK_TEMPLATE_ID   = "TMPL6WFM0Gz6O"
 BLYNK_TEMPLATE_NAME = "MQTT Demo"
-BLYNK_AUTH_TOKEN    = "Xu0R-PFMnWfs1oZYPDo43vVKdjiVvhVw"
+BLYNK_AUTH_TOKEN    = "1m5AmtbTUnEnfpZ5UKuF4CHUy3rRcMeL"
 
 # 變更預設 Blynk 伺服器。
 BLYNK_MQTT_BROKER   = "blynk.cloud"
